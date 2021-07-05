06fa117bc5ae2b62de55c7a0434bfa0d5930451a
liguannan <liguannan@koolearn-inc.com>
2021-07-05 11:52:21 +0800

    feature: init program

---
+++ learn-practice/src/main/java/com/geek/learn/LearnPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.geek.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnPracticeApplication.class, args);
+    }
+
+}
---
+++ learn-practice/src/test/java/com/geek/learn/LearnPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
